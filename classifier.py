f77d2ff8b2e6dab877f86a6e04fc1f3bd3798697
Alexander Nesterenko <nestorf250@gmail.com>
2016-02-15 16:02:43 +0200

    Implement classifier

--- classifier.py
+++ classifier.py
@@ -5,5 +5,32 @@ class Classifier():
     def __init__(self):
         super(Classifier, self).__init__()
 
-    def classify(data):
-        # TODO: I'm pretending to write something down
+    def classify(self, data):
+        wrong_count = 0
+        for item in data:
+            selected = ""
+            if (
+                    (item['vals'][0] > 4.0 and item['vals'][0] < 6.0) and
+                    (item['vals'][1] > 2.2 and item['vals'][1] < 4.5) and
+                    (item['vals'][2] > 1.0 and item['vals'][2] < 2.0) and
+                    (item['vals'][3] > 0 and item['vals'][3] < 0.6)):
+                selected = "Iris-setosa"
+            elif (
+                    (item['vals'][0] > 4.5 and item['vals'][0] < 7.3) and
+                    (item['vals'][1] > 2.1 and item['vals'][1] < 3.7) and
+                    (item['vals'][2] > 3.0 and item['vals'][2] < 5.6) and
+                    (item['vals'][3] > 0.8 and item['vals'][3] < 1.8)):
+                selected = "Iris-versicolor"
+            elif (
+                    (item['vals'][0] > 4.9 and item['vals'][0] < 8.3) and
+                    (item['vals'][1] > 2.1 and item['vals'][1] < 3.9) and
+                    (item['vals'][2] > 4.3 and item['vals'][2] < 6.9) and
+                    (item['vals'][3] > 1.4 and item['vals'][3] < 2.6)):
+                selected = "Iris-virginica"
+            print("[%.1f %.1f %.1f %.1f] classified as %s, expected %s" % (
+                item['vals'][0], item['vals'][1],
+                item['vals'][2], item['vals'][3],
+                selected, item['class']))
+            if selected != item['class']:
+                wrong_count += 1
+        print("Wrong recognized count is %i" % wrong_count)
--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-# from classifier import Classifier
+from classifier import Classifier
 
 
 def main():
-    print("Hello world!!!")
-    # sifier = Classifier()
-    read_from_file(name='iris.data')
+    sifier = Classifier()
+    data = read_from_file(name='iris.data')
+    # display_correlation(data)
+    classifier = Classifier()
+    classifier.classify(data)
     return 0
+
+
+def display_correlation(data):
+    current_class = data[0]["class"]
+    current_class_count = 0
+    current_class_max_diff = [0.0, 0.0, 0.0, 0.0]
+    current_class_values = [0.0, 0.0, 0.0, 0.0]
+    for item in data:
+        if current_class == item['class']:
+            for i in range(len(item['vals'])):
+                if current_class_count != 0:
+                    current_class_max_diff[i] = (
+                        current_class_max_diff[i]
+                        if current_class_max_diff[i] > abs(
+                            (current_class_values[i] / current_class_count) -
+                            item['vals'][i]
+                        ) else abs(
+                            (current_class_values[i] / current_class_count) -
+                            item['vals'][i]
+                        )
+                    )
+                current_class_values[i] += item['vals'][i]
+            current_class_count += 1
+        else:
+            print(" Class is %s, middle values are:" % current_class)
+            for i in range(len(current_class_values)):
+                print("\t%.1f +- %.1f" % (
+                    current_class_values[i]/current_class_count,
+                    current_class_max_diff[i]))
+            current_class = item['class']
+            current_class_count = 1
+            current_class_values = item['vals']
+            current_class_max_diff = [0.0, 0.0, 0.0, 0.0]
+    print(" Class is %s, middle values are:" % current_class)
+    for i in range(len(current_class_values)):
+        print("\t%.1f +- %.1f" % (
+            current_class_values[i]/current_class_count,
+            current_class_max_diff[i]))
+    current_class = item['class']
+    current_class_count = 1
+    current_class_values = item['vals']
+    current_class_max_diff = [0.0, 0.0, 0.0, 0.0]
 
 
 def read_from_file(name=""):
@@ -21,11 +65,8 @@ def read_from_file(name=""):
                     point["vals"].append(float(value))
                 except ValueError:
                     point["class"] = value
-            data.append(point)
-    print(data)
-    print(len(data))
-    # TODO: write file
-    data_file.close()
+            if len(point["vals"]) == 4 and "class" in point:
+                data.append(point)
     return data
 
 if __name__ == "__main__":
