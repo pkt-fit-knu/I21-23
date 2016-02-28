05f3ad660cb8a68753f25c0a170c59691eeb1d83
Alexander Nesterenko <nestof250@gmail.com>
2016-02-28 13:12:42 +0200

    Implement billinear interpolation algorythm
    - Update `README.md` of lab1

--- lab1/README.md
+++ lab1/README.md
@@ -2,11 +2,24 @@
 
 Implements lab 1 from image processing course. 
 
+### How to run
+Since there is some libraries needed to run it, run those steps to run the lab
+```bash
+virtualenv --clear --prompt="[lab1]" .env
+source .env/bin/activate
+pip install -r requirements.txt
+```
+Then you can just type `python main.py`. It could take some time to transform picture so I have already provided resized one.
+
 ### What does it does
 
-It implements resizing of [picture](pic1.png) on bilinear interpolation algorythms.
+It implements resizing of [picture](pic1.jpg) on billinear interpolation algorythms.
 
 ### File walkthrough
 
-*not implemented yet*
+*not needed - everything is in main.py file*
+
+### Result of lab
+
+*placeholder*
 
--- lab1/main.py
+++ lab1/main.py
@@ -1,8 +1,59 @@
-"""A program that implements classifying data by primitive algorythms."""
+"""A program that implements resizing image by bilinear algorythm."""
+
+import imageio
+import numpy as np
 
 
 def main():
     """Main program method."""
+    img = imageio.imread("pic1.jpg")
+
+    new_img = np.zeros((
+        len(img) * 2,
+        len(img[0]) * 2,
+        len(img[0][0])
+    ), dtype=np.uint32)
+
+    for y_pos in range(len(img) - 1):
+        for x_pos in range(len(img[0]) - 1):
+            apper_left = img[y_pos][x_pos]
+            apper_right = img[y_pos][x_pos + 1]
+            bottom_left = img[y_pos + 1][x_pos]
+            bottom_right = img[y_pos + 1][x_pos + 1]
+
+            for i in range(len(img[0][0])):
+                # First level of interpolation - linear, first row
+                new_img[y_pos * 2 - 1][x_pos * 2 - 1][i] = apper_left[i]
+                new_img[y_pos * 2 - 1][x_pos * 2 + 1][i] = apper_right[i]
+                new_img[y_pos * 2 - 1][x_pos * 2 + 1][i] = (
+                    (apper_left[i] / 2 + apper_right[i] / 2)
+                )
+
+                # First level of interpolation - linear, second row
+                new_img[y_pos * 2 + 1][x_pos * 2][i] = bottom_left[i]
+                new_img[y_pos * 2 + 1][x_pos * 2 + 1][i] = bottom_right[i]
+                new_img[y_pos * 2 + 1][x_pos * 2][i] = (
+                    (bottom_left[i] / 2 + bottom_right[i] / 2)
+                )
+
+                # First level of interpolation - linear, first column
+                new_img[y_pos * 2][x_pos * 2 - 1][i] = (
+                    (apper_left[i] / 2 + bottom_left[i] / 2)
+                )
+
+                # First level of interpolation - linear, second column
+                new_img[y_pos * 2 + 1][x_pos * 2][i] = (
+                    (apper_right[i] / 2 + bottom_right[i] / 2)
+                )
+
+                # Second level of interpolation - linear, middle column
+
+                new_img[y_pos * 2][x_pos * 2][i] = ((
+                    new_img[y_pos * 2 - 1][x_pos * 2 - 1][i] / 2 +
+                    new_img[y_pos * 2 + 1][x_pos * 2 + 1][i] / 2
+                ))
+        print "%i/%i" % (y_pos, len(img) - 1)
+    imageio.imwrite("pic1_bigger.png", new_img)
 
 
 if __name__ == "__main__":
